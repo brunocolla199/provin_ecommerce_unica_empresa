3231b653d80588eeab34d8550ad16666b96ab23a
brunocolla <brunocolla199@gmail.com>
2020-11-02 09:01:20 -0300

    ajustes

--- resources/views/ecommerce/produto/index.blade.php
+++ resources/views/ecommerce/produto/index.blade.php
@@ -608,4 +608,35 @@
 
 @section('footer')
 <script src="{{ asset('controllers/produto.js') }}"></script>
+<script>
+    $('.btn-add-cart').on('click',function(){
+        var id   = $(this).data('id');
+        var tipo = $(this).data('tipo');
+        var tamanho = $('#tamanho-'+id).val();
+        var descricaoCarrinho = tipo == 'express' ? ' expresso' : ' de compras';
+
+        let add_carrinho = swal2_warning("Essa ação irá adicionar o produto ao carrinho"+descricaoCarrinho ,"Sim!");
+        let obj = {'id': id};
+
+        add_carrinho.then(resolvedValue => {
+            $.ajax({
+                type: "POST",
+                url: 'produto/adicionarCarinho',
+                data: { id: id, tipo: tipo, tamanho:tamanho,quantidade:1, _token: '{{csrf_token()}}' },
+                success: function (data) {
+                    if(data.response != 'erro') {
+                        swal2_success("Adicionado !", "Produto adicionado com sucesso.");
+                    } else {
+                        swal2_alert_error_support("Tivemos um problema ao adicionar o produto.");
+                    }
+                },
+                error: function (data, textStatus, errorThrown) {
+                    console.log(data);
+                },
+            });
+        }, error => {
+            swal.close();
+        });
+    });
+</script>
 @endsection
