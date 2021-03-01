ed733f7e16f393a0acb4a9b3a55d9cbad4842e5f
brunocolla <brunocolla199@gmail.com>
2021-03-01 19:33:48 -0300

    teste

--- resources/views/ecommerce/menu/menu-home.blade.php
+++ resources/views/ecommerce/menu/menu-home.blade.php
@@ -7,28 +7,28 @@
     <div>
         <a href="{{route('ecommerce.home')}}">
             <img  class="@if ($explode[2] == 'home') borda @endif iconeHome" src="{{asset('img/icones/s1.1.png')}}">
-            <span style="display: flex;justify-content: center;font-size: 12px"> Início </span>
+            <span style="display: flex;justify-content: center;font-size: 12px;width: 80%"> Início </span>
         </a>
         
     </div>
     <div>
         <a href="{{route('ecommerce.produto')}}">
             <img  class="@if ($explode[2] == 'produto') borda @endif iconeProduto" src="{{asset('img/icones/s2.2.png')}}">
-            <span style="display: flex;justify-content: center;font-size: 12px"> Produtos </span>
+            <span style="display: flex;justify-content: center;font-size: 12px;width: 80%"> Produtos </span>
         </a>
         
     </div>
     <div>
         <a href="{{route('ecommerce.pedido')}}">
             <img  class="@if ($explode[2] == 'pedido') borda @endif iconePedido" src="{{asset('img/icones/s3.3.png')}}">
-            <span style="display: flex;justify-content: center;font-size: 12px"> Pedidos </span>
+            <span style="display: flex;justify-content: center;font-size: 12px;width: 80%"> Pedidos </span>
         </a>
         
     </div>
     <div>
         <a href="{{route('ecommerce.estoque')}}">
             <img  class="@if ($explode[2] == 'estoque') borda @endif iconeEstoque" src="{{asset('img/icones/s4.4.png')}}">
-            <span style="display: flex;justify-content: center;font-size: 12px"> Estoque </span>
+            <span style="display: flex;justify-content: center;font-size: 12px;width: 80%"> Estoque </span>
         </a>
         
     </div>
